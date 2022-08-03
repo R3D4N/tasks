8c0dabfd16401040a4b45dda369aea3d200c6055
R3_D4N <bellidoangelo97@gmail.com>
2022-08-02 23:03:28 -0500

    resumiendo sentencia if

--- tema5/funciones_eje3.py
+++ tema5/funciones_eje3.py
@@ -16,4 +16,8 @@ if isLeap(year):
 else:
     print("no es bisiesto")
 
-# if year%4==0 and (year%100!=0 or year%400==0):
+""" if year%4==0 and (year%100!=0 or year%400==0):
+        return true
+    else:
+        return false
+"""
