e06d5a0b52dc5e9c65f5cb4670b1e05f4a664aca
R3_D4N <bellidoangelo97@gmail.com>
2022-09-13 21:44:20 -0500

    ejercicio 1 tema 9 funciones built-in

---
+++ tema9/ejercicio1/main.py
@@ -0,0 +1,14 @@
+def main():
+    countries=input("Paises separados por coma: ")
+    lstCountries=set(countries.split(","))
+    # borra los espacios innecesario de cada elemento
+    lastList=list(map(noEspacio,lstCountries))
+    lastList.sort()
+    for item in lastList:
+        print(item,sep=", ")
+
+def noEspacio(item="a"):
+    return item.lstrip()
+    
+if __name__=="__main__":
+    main()
