77aaebd8a3fdcd40248a90e3a012c91ce15439ac
R3_D4N <bellidoangelo97@gmail.com>
2022-09-13 22:25:05 -0500

    ejercio 2 tema 9 funciones built-in solved

--- tema9/ejercicio2/main.py
+++ tema9/ejercicio2/main.py
@@ -1,5 +1,11 @@
+from functools import reduce
+
 def main():
-    pass
+    lista=[1,2,3,4,5,6,7,8,9]
+    listaImpar=list(filter(lambda x:x%2!=0,lista))
+    sumaImpar=reduce(lambda a,b:a+b,listaImpar)
+    print("La suma de ",listaImpar," es ",sumaImpar)
+
 
 if __name__=="__main__":
     main()
